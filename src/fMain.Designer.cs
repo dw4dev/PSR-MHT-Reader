73e42d78818aa76eaef2d2966a3d9491fa13639a
Davidson <davidson@lwsys.com.tw>
2022-12-26 18:04:26 +0800

    完成拖拉mht檔案並瀏覽顯示

--- src/fMain.Designer.cs
+++ src/fMain.Designer.cs
@@ -29,21 +29,102 @@ namespace PsrMhtReader
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.txtFilePath = new System.Windows.Forms.TextBox();
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnOpenFile);
+            this.groupBox1.Controls.Add(this.txtFilePath);
+            this.groupBox1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.groupBox1.Location = new System.Drawing.Point(0, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(791, 46);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Source File";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(6, 16);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(32, 23);
+            this.btnOpenFile.TabIndex = 3;
+            this.btnOpenFile.Text = "...";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            // 
+            // txtFilePath
+            // 
+            this.txtFilePath.AllowDrop = true;
+            this.txtFilePath.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtFilePath.BackColor = System.Drawing.Color.OldLace;
+            this.txtFilePath.Font = new System.Drawing.Font("Consolas", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFilePath.ForeColor = System.Drawing.Color.MediumBlue;
+            this.txtFilePath.Location = new System.Drawing.Point(44, 15);
+            this.txtFilePath.Name = "txtFilePath";
+            this.txtFilePath.ReadOnly = true;
+            this.txtFilePath.Size = new System.Drawing.Size(735, 25);
+            this.txtFilePath.TabIndex = 0;
+            this.txtFilePath.Text = "ttttt";
+            this.txtFilePath.DragDrop += new System.Windows.Forms.DragEventHandler(this.ProcessFile_DragDrop);
+            this.txtFilePath.DragEnter += new System.Windows.Forms.DragEventHandler(this.ProcessFileDrop_DragEnter);
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = false;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 61);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(791, 389);
+            this.webView21.Source = new System.Uri("about:blank", System.UriKind.Absolute);
+            this.webView21.TabIndex = 1;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 51);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(791, 10);
+            this.panel1.TabIndex = 2;
             // 
             // fMain
             // 
-            this.AllowDrop = true;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(791, 450);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.groupBox1);
             this.Name = "fMain";
+            this.Padding = new System.Windows.Forms.Padding(0, 5, 0, 0);
             this.Text = "Problem Steps Recorder - MHT Reader";
+            this.Load += new System.EventHandler(this.fMain_Load);
+            this.DragDrop += new System.Windows.Forms.DragEventHandler(this.ProcessFile_DragDrop);
+            this.DragEnter += new System.Windows.Forms.DragEventHandler(this.ProcessFileDrop_DragEnter);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.TextBox txtFilePath;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnOpenFile;
     }
 }
 
--- src/fMain.cs
+++ src/fMain.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,163 @@ namespace PsrMhtReader
         public fMain()
         {
             InitializeComponent();
+            txtFilePath.Text = "";
         }
+
+        private void fMain_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ProcessFile_DragDrop(object sender, DragEventArgs e)
+        {
+            object ofiles = e.Data.GetData(DataFormats.FileDrop);
+            if (ofiles is string[] paths)
+            {
+                string mht = paths.FirstOrDefault();
+                if (!string.IsNullOrEmpty(mht))
+                {
+                    string ext = Path.GetExtension(mht).ToUpper();
+                    if (ext == ".MHT")
+                    {
+                        txtFilePath.Text = mht;
+                        ProcessMHTFile(mht);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Only filenames with .mht are accepted.", "Incorrect file",
+                            MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            e.Effect = DragDropEffects.None;
+        }
+
+        private void ProcessFileDrop_DragEnter(object sender, DragEventArgs e)
+        {
+            e.Effect = DragDropEffects.Link;
+        }
+
+        void ProcessMHTFile(string mht_path)
+        {
+            //FileInfo mht = new FileInfo(mht_path);
+            string content = File.ReadAllText(mht_path);
+            string[] lines = content.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+
+
+            string tmpname = Path.GetTempFileName().Replace(".tmp", "");
+            string output_path = Path.Combine(Path.GetTempPath(), tmpname);
+            if (!Directory.Exists(output_path))
+                Directory.CreateDirectory(output_path);
+
+            #region Disassemble MHT File
+            string boundary = "", boundaryEnd = "";
+            int lineidx = 0;
+            #region Find boundary string
+            for (; lineidx < lines.Length; lineidx++)
+            {
+                string item = lines[lineidx];
+
+                if (item.Contains("boundary"))
+                {
+                    int pos1 = item.IndexOf("\"", 0);
+                    int pos2 = item.IndexOf("\"", pos1 + 1);
+                    boundary = $"--{item.Substring(pos1 + 1, (pos2 - pos1 - 1))}";
+                    boundaryEnd = boundary + "--";
+                    break;
+                }
+                if (lineidx > 10)
+                {
+                    MessageBox.Show("Unable to disassemble MHT file.", "Unknow mht file.",
+                        MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+                if (item == boundary) break;
+            }
+            #endregion
+
+            lineidx++;
+            string ContentType = "";
+            string Charset = "";
+            string FileName = "";
+            string TransferEncoding = "";
+            List<string> subContent = new List<string>();
+            for (; lineidx < lines.Length; lineidx++)
+            {
+                string item = lines[lineidx];
+                if (item.StartsWith("Content-Type"))
+                {
+                    string[] parts = item.Replace(" ", "").Replace("\"", "")
+                        .Split(new string[] { ":", ";", "=" }, StringSplitOptions.RemoveEmptyEntries);
+                    ContentType = parts[1];
+                    Charset = (item.Contains("charset") ? parts[3] : "");
+                    continue;
+                }
+                if (item.StartsWith("Content-Transfer-Encoding"))
+                {
+                    string[] parts = item.Split(':');
+                    TransferEncoding = parts[1].Trim();
+                    continue;
+                }
+                if (item.StartsWith("Content-Location"))
+                {
+                    string[] parts = item.Split(':');
+                    FileName = parts[1].Trim();
+                    continue;
+                }
+
+                if (item == boundary || item == boundaryEnd)
+                {
+                    switch (ContentType)
+                    {
+                        case "text/html":
+                        case "text/css":
+                            WriteTextFile(Path.Combine(output_path, FileName), Encoding.UTF8, subContent);
+                            break;
+                        case "image/jpeg":
+                            WriteImageFile(Path.Combine(output_path, FileName), TransferEncoding, subContent);
+                            break;
+                    }
+
+                    if (item == boundaryEnd) break;
+
+                    subContent.Clear();
+                }
+                else
+                    subContent.Add(item);
+            }
+            #endregion
+
+            string startUrl = "file:///" + $@"{output_path.Replace("\\", "/")}/main.htm";
+
+            webView21.Source = new Uri(startUrl);
+        }
+
+        void WriteTextFile(string fn, Encoding encoding, List<string> content)
+        {
+            StreamWriter sw = new StreamWriter(fn, false, encoding);
+            foreach (var item in content)
+            {
+                sw.WriteLine(item);
+            }
+            sw.Flush();
+            sw.Close();
+        }
+
+        void WriteImageFile(string fn, string transferEncoding, List<string> content)
+        {
+            byte[] img = null;
+            if (transferEncoding == "base64")
+            {
+                string allcontent = string.Join("", content);
+                img = Convert.FromBase64String(allcontent);
+            }
+            else return; //DO NOTHING
+
+            BinaryWriter bw = new BinaryWriter(File.OpenWrite(fn));
+            bw.Write(img);
+            bw.Close();
+        }
+
     }
 }
